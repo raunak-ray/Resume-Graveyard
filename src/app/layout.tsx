28917e60706b5d9c7ec47ec9184ca7c886fe7b21
Raunak Ray <raunakay3260@gmail.com>
2026-05-17 18:38:40 +0530

    feat: created navbar components with fully responsive across all devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Creepster, Geist, Geist_Mono, Great_Vibes, Nosifer } from "next/font/google";
 
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -46,7 +47,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} ${nosifier.variable} ${creepster.variable} ${greatVibes.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,7 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='text-3xl font-nosifer text-red-400'>
-      HELLO
-      <h1 className='text-3xl font-creepster text-red-400'>
-        BYE
-      </h1>
-      <h1 className='text-3xl font-great-vibes text-red-400'>
-        Vibes
-      </h1>
-      <div className='bg-card text-card-foreground'>
-        hello
-      </div>
-    </div>
+    <div className='flex-1'>page</div>
   )
 }
 

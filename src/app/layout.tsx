dc101b86f743257972f0a65d37e0023d8feacfa3
Raunak Ray <raunakay3260@gmail.com>
2026-05-17 18:07:56 +0530

    feat: initialize project with shadcn/ui components, tailwind configuration, and custom Google fonts.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Creepster, Geist, Geist_Mono, Great_Vibes, Nosifer } from "next/font/google";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -7,10 +8,28 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
+const nosifier = Nosifer({
+  weight:"400",
+  subsets: ["latin"],
+  variable:"--font-nosifer",
+})
+
+const creepster = Creepster({
+  weight:"400",
+  subsets: ["latin"],
+  variable:"--font-creepster",
+})
+
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const greatVibes = Great_Vibes({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-great-vibe",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +44,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} ${nosifier.variable} ${creepster.variable} ${greatVibes.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
